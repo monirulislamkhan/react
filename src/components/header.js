f479bc786de92b5832734de1d7c752860c8a727c
monirulislamkhan <mr.monirulkhan@gmail.com>
2024-04-28 16:30:49 +0530

    work initiate from home

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import { Suspense } from "react";
+
+export default function Header() {
+  return (
+    <>
+      <Suspense>
+        <h2>This is Header</h2>
+      </Suspense>
+    </>
+  );
+}
---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from '../components/header'
+export default function About() {
+  const [sticky, setSticky] = useState(0);
+  // const clickHandle = () => setSticky(sticky + 1);
+  // function clickHandle() {
+  //   setSticky(sticky + 1)
+  // }
+
+  console.log('Hello, my name is monirul')
+
+  return <>
+    <Header />
+    <h1>About Page {sticky}</h1>
+    <button onClick={(e) => setSticky(sticky + 1)}>Click Me</button>
+  </>;
+}
